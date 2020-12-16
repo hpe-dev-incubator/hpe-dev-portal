8983d97760083272c757e3f955ecc8c326b7b466
pramod sareddy <reddypramod85>
2020-12-15 20:49:52 -0600

    fix review comments

--- src/containers/EmailCapture/index.js
+++ src/containers/EmailCapture/index.js
@@ -11,51 +11,58 @@ import {
   Button,
 } from 'grommet';
 
+const emailValidation = [
+  {
+    regexp: new RegExp('[^@ \\t\\r\\n]+@'),
+    message: 'Enter a valid email address.',
+    status: 'error',
+  },
+  {
+    regexp: new RegExp('[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+'),
+    message: 'Enter a valid email address.',
+    status: 'error',
+  },
+  {
+    regexp: new RegExp('[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+'),
+    message: 'Enter a valid email address.',
+    status: 'error',
+  },
+];
+
 export const EmailCapture = () => {
   const [succeeded, setSucceeded] = useState(false);
   const [error, setError] = useState('');
-  const [emailError, setEmailError] = useState('');
   const [formData, setFormData] = useState({
     email: '',
   });
 
-  const isValidEmail = (email) => {
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
-    return emailRegex.test(email);
-  };
-
-  const onSubmit = () => {
+  const onSubmit = ({ data, touched }) => {
     // eslint-disable-line
-    if (isValidEmail(formData.email)) {
-      setEmailError('');
-      setSucceeded(false);
-      setError('');
-      //  fetch(`https://api.sendgrid.com/v3/contactdb/lists/${list_id}/recipients/${recipient_id}`, {
-      //   method: 'POST',
-      //   headers: new Headers({ // eslint-disable-line
-      //     'Content-Type': 'application/json'
-      //   }),
-      //   body: JSON.stringify( data.emailAddress, listId )
-      // })
-      //   .then(response =>
-      //     response.json().then(json => ({
-      //       status: response.status,
-      //       statusText: response.statusText,
-      //       json,
-      //     })),
-      //   )
-      //   .then(({ status, json }) => {
-      //     if (status >= 400) {
-      //       setError(json.error ||
-      //                  'There was an error processing your request.');
-      //       setSucceeded(false)
-      //     }
+    setSucceeded(false);
+    setError('');
+    //  fetch(`https://api.sendgrid.com/v3/contactdb/lists/${list_id}/recipients/${recipient_id}`, {
+    //   method: 'POST',
+    //   headers: new Headers({ // eslint-disable-line
+    //     'Content-Type': 'application/json'
+    //   }),
+    //   body: JSON.stringify( data.emailAddress, listId )
+    // })
+    //   .then(response =>
+    //     response.json().then(json => ({
+    //       status: response.status,
+    //       statusText: response.statusText,
+    //       json,
+    //     })),
+    //   )
+    //   .then(({ status, json }) => {
+    //     if (status >= 400) {
+    //       setError(json.error ||
+    //                  'There was an error processing your request.');
+    //       setSucceeded(false)
+    //     }
 
-      //       setSucceeded(true);
-      //   });
-    } else {
-      setEmailError('Email is not valid');
-    }
+    //       setSucceeded(true);
+    //   });
   };
 
   const resetForm = () => {
@@ -71,7 +78,7 @@ export const EmailCapture = () => {
           HPE Developer Newsletter
         </Heading>
       </Box>
-      <Box align="start">
+      <Box>
         <Text>Stay in the loop.</Text>
         <Text style={{ marginTop: 0 }}>
           Sign up for the HPE Developer Newsletter or visit the{' '}
@@ -84,14 +91,14 @@ export const EmailCapture = () => {
           validate="blur"
           value={formData}
           onChange={setFormData}
-          onSubmit={({ value }) => onSubmit({ value })}
+          onSubmit={({ value, touched }) => onSubmit({ value, touched })}
         >
           <FormField
             name="email"
             label="Email Address"
-            error={emailError}
             required
             width="medium"
+            validate={emailValidation}
           >
             <TextInput placeholder="example@my.com" name="email" />
           </FormField>
@@ -115,16 +122,16 @@ export const EmailCapture = () => {
             />
             {error && (
               <Paragraph size="large" margin="none">
-                {error && 'There was an error storing your email.'}
+                {error && 'Unable to subscribe to the newsletter.Try again!'}
               </Paragraph>
             )}
           </Box>
         </Form>
       </Box>
       {succeeded && (
-        <Box pad={{ between: 'medium' }}>
+        <Box>
           <Paragraph size="large" margin="none">
-            Thanks! We have successfully stored your email address.
+            Thanks! You are subscribed to the newsletter.
           </Paragraph>
           <Paragraph size="large" margin="none">
             <Anchor onClick={resetForm}>Reset entry form</Anchor>
