1dae7998ce5b1eec2d8a3f0db47ada69d918e94a
pramod sareddy <reddypramod85>
2020-12-16 21:54:50 -0600

    change links to anchor and update spacing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,84 +1,88 @@
 import React from 'react';
-import { Box, Heading, Button } from 'grommet';
+import { Box, Heading, Anchor } from 'grommet';
 import { EmailCapture } from '../../containers';
 
 export const Footer = () => (
-  <Box direction="row-responsive" border justify="between">
-    <Box pad={{ vertical: 'large', horizontal: 'large' }}>
+  <Box
+    direction="row-responsive"
+    border
+    justify="between"
+    pad={{ vertical: 'large', horizontal: 'xlarge' }}
+  >
+    <Box>
       <EmailCapture />
     </Box>
-    <Box
-      direction="row"
-      justify="between"
-      pad={{ vertical: 'large', horizontal: 'xlarge' }}
-      gap="xlarge"
-    >
-      <Box align="start" gap="small">
+    <Box direction="row" justify="between" gap="large">
+      <Box align="start" gap="medium">
         <Heading margin="none" level="4">
           HPE Dev
         </Heading>
-        <Button
-          align="start"
-          href="https://slack.hpedev.io/"
-          label="Slack Channel"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-        <Button
-          align="start"
-          href="https://twitter.com/HPE_DevCom"
-          label="Twitter"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-        <Button
-          align="start"
-          href="https://www.youtube.com/channel/UCAdEqOhSnzlLBtaapu567AQ"
-          label="YouTube"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-        <Button
-          align="start"
-          href="mailto:hpedev@hpe.com?
-          subject=HPE%20DEV%20Portal%20-%20Contact%20Us"
-          label="Contact Us"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
+        <Box gap="small">
+          <Anchor
+            align="start"
+            href="https://slack.hpedev.io/"
+            label="Slack Channel"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Anchor
+            align="start"
+            href="https://twitter.com/HPE_DevCom"
+            label="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Anchor
+            align="start"
+            href="https://www.youtube.com/channel/UCAdEqOhSnzlLBtaapu567AQ"
+            label="YouTube"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Anchor
+            align="start"
+            href="mailto:hpedev@hpe.com?
+            subject=HPE%20DEV%20Portal%20-%20Contact%20Us"
+            label="Contact Us"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+        </Box>
       </Box>
-      <Box align="start" gap="small">
+      <Box align="start" gap="medium">
         <Heading margin="none" level="4">
           About HPE
         </Heading>
-        <Button
-          align="start"
-          href="https://www.hpe.com/"
-          label="HPE.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-        <Button
-          align="start"
-          href="https://www.hpe.com/us/en/about/jobs.html"
-          label="HPE Careers"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-        <Button
-          align="start"
-          href="https://community.hpe.com/"
-          label="HPE Community"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-        <Button
-          align="start"
-          href="https://github.com/hewlettpackard/"
-          label="HPE GitHub"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
+        <Box gap="small">
+          <Anchor
+            align="start"
+            href="https://www.hpe.com/"
+            label="HPE.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Anchor
+            align="start"
+            href="https://www.hpe.com/us/en/about/jobs.html"
+            label="HPE Careers"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Anchor
+            align="start"
+            href="https://community.hpe.com/"
+            label="HPE Community"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Anchor
+            align="start"
+            href="https://github.com/hewlettpackard/"
+            label="HPE GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+        </Box>
       </Box>
     </Box>
   </Box>
