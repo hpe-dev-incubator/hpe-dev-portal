d298a64cd01ad4040e61556302b292d832c26708
pramod sareddy <reddypramod85>
2021-02-03 23:06:27 -0600

    update blog subpage with latest design

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -1,47 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Anchor, Box, Text } from 'grommet';
-import { Facebook, Linkedin, Twitter } from 'grommet-icons';
+import { Box, Button } from 'grommet';
+import { Linkedin, Twitter, Link } from 'grommet-icons';
 
-export const Share = ({ url, text }) => (
-  <Box gap="small" justify="center">
-    <Text size="large">Share</Text>
-    <Anchor
-      target="_blank"
-      rel="noopener noreferrer"
-      a11yTitle="Share on Twitter"
-      href={`https://twitter.com/intent/tweet?url=${url}&text=${text}`}
-    >
-      <Box direction="row" align="center" gap="small">
-        <Twitter size="medium" />
-        <Text>Twitter</Text>
-      </Box>
-    </Anchor>
-    <Anchor
-      target="_blank"
-      rel="noopener noreferrer"
-      a11yTitle="Share on Facebook"
-      href={`https://www.facebook.com/dialog/share?app_id=199460010977271&display=popup&href=${url}`}
-    >
-      <Box direction="row" align="center" gap="small">
-        <Facebook size="medium" />
-        <Text>Facebook</Text>
-      </Box>
-    </Anchor>
-    <Anchor
-      target="_blank"
-      rel="noopener noreferrer"
-      a11yTitle="Share on LinkedIn"
-      href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
-    >
-      <Box direction="row" align="center" gap="small">
-        <Linkedin size="medium" />
-        <Text>LinkedIn</Text>
-      </Box>
-    </Anchor>
-  </Box>
-);
+export const Share = ({ url, text }) => {
+  const [toolTip, setToolTip] = useState('Click to copy the URL to clipboard');
+  return (
+    <Box gap="small" direction="row" justify="start">
+      <Button
+        target="_blank"
+        rel="noopener noreferrer"
+        a11yTitle="Share on Twitter"
+        href={`https://twitter.com/intent/tweet?url=${url}&text=${text}`}
+        icon={<Twitter />}
+      />
+      <Button
+        target="_blank"
+        rel="noopener noreferrer"
+        a11yTitle="Share on LinkedIn"
+        href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`}
+        icon={<Linkedin />}
+      />
+      <Button
+        icon={<Link />}
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setToolTip('Copied!');
+        }}
+        tip={{
+          dropProps: { align: { left: 'right' } },
+          content: toolTip,
+        }}
+      />
+    </Box>
+  );
+};
 
 Share.propTypes = {
   url: PropTypes.string,
