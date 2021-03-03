dcb2e84a27fb0c56765316072c4772fbc7d63a92
pramod sareddy <reddypramod85>
2021-03-03 16:36:26 -0600

    add cloudinary signature

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,16 @@
-import CMS from 'netlify-cms-app';
+window.CMS_MANUAL_INIT = true;
+import CMS, { init } from 'netlify-cms-app';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
+
+init({
+  config: {
+    media_library: {
+      config: {
+        signature:
+          'c8cbf1fd4353ff060aa4190008209b07ad8a6f2c3d7b09e4b7238363761d8cdd',
+      },
+    },
+  },
+});
 
 CMS.registerMediaLibrary(cloudinary);
