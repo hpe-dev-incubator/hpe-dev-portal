3191fe8cb15c9d58250b566a8a360fedcd4b5183
pramod sareddy <reddypramod85>
2021-03-09 23:00:49 -0600

    add 404 page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Heading, Paragraph } from 'grommet';
+import { Box, Button, Heading, Image } from 'grommet';
+import { navigate } from 'gatsby';
 import { Layout, SEO } from '../../components';
 import { useSiteMetadata } from '../../hooks/use-site-metadata';
 
@@ -9,8 +10,47 @@ function NotFoundPage() {
     <Layout title={siteMetadata.siteTitle}>
       <Box flex align="center" justify="center">
         <SEO title="404: Not Found" />
-        <Heading>Not Found</Heading>
-        <Paragraph>This page doesn&#39;t exist... the sadness.</Paragraph>
+      </Box>
+      <Box flex align="center" justify="center" gap="large">
+        <Box direction="row" align="center" justify="center" gap="xlarge">
+          <Box flex direction="row">
+            <Heading margin="none">Error 4</Heading>
+            <Image
+              style={{
+                maxWidth: 'initial',
+                height: '50px',
+                width: '50px',
+              }}
+              src="/img/404 developer.svg"
+            />
+            <Heading margin="none">4</Heading>
+          </Box>
+          <Box>
+            <Image
+              src="/img/hpe-dev-it-monster-worried.svg"
+              style={{
+                maxWidth: 'initial',
+                height: '450px',
+                width: '350px',
+              }}
+            />
+          </Box>
+        </Box>
+        <Box
+          pad="medium"
+          flex
+          align="center"
+          justify="center"
+          fill="horizontal"
+          background={{ color: 'red' }}
+        >
+          <Heading level="4" margin="none">
+            Sorry, we couldn't find what you were looking for.
+          </Heading>
+        </Box>
+        <Box align="center" pad="medium">
+          <Button label="Go Back" secondary onClick={() => navigate('/')} />
+        </Box>
       </Box>
     </Layout>
   );
