889a1276ff7e28416882f09e803928a163e46eb7
Johnathan Giang <jayhgiang@gmail.com>
2021-05-03 12:43:56 -0700

    Add styles inline

--- src/cms/imageUploadControl.js
+++ src/cms/imageUploadControl.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+
 export default class ImageUploadControl extends Component {
   constructor(props) {
     super(props);
