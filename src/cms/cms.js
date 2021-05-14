ad3d53bf1b8cd0b98f51260a01d273e0c310058c
pramod sareddy <reddypramod85>
2021-05-13 23:20:14 -0500

    add netlify function

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,12 +2,27 @@ window.CMS_MANUAL_INIT = true;
 import CMS, { init } from 'netlify-cms-app';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 
+let myJson;
+const userAction = async () => {
+  try {
+    const response = await fetch(
+      'https://hackshack.hpedev.io/.netlify/functions/cloudinary',
+    );
+    myJson = await response.json();
+    console.log('netlify function response', myJson);
+  } catch (err) {
+    console.log('netlify function call error', err);
+  }
+};
+
+userAction();
+
 init({
   config: {
     media_library: {
       config: {
         signature:
-          '428942879918aae8eb9390200af32ebacdb593601e2956d00506d12d067e0464',
+          '7e1bd971329791e8348f729680a66ca171fc2024c395f26ec4e82b14ff2c289e',
       },
     },
   },
