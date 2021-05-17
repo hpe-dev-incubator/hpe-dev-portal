4980cc30aa7e2dd41526c345269d9ce7aec82672
Johnathan Giang <jayhgiang@gmail.com>
2021-05-16 18:45:07 -0700

    Update css style for code tag

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -28,6 +28,15 @@ const MarkdownLayout = styled(Markdown)`
     margin-top: 0;
     padding-top: 0;
   }
+  
+  & > p > code {
+    color: rgb(248, 248, 242);
+    background: rgb(43, 43, 43);
+    font-family: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace;
+    text-align: left;
+    white-space: pre;
+    font-size: 20px;
+  }
 `;
 
 const columns = {
