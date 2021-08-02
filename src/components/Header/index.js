152289e7b0f4588cc29a4665bd62d3a122e27132
jhoun <jayhgiang@gmail.com>
2021-08-02 08:46:19 -0700

    Use DropButton grommet component to display platforms nav items

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,11 +17,116 @@ function Header() {
       label="Open Source"
       to="https://www.hpe.com/us/en/open-source.html"
     />,
-    <ButtonLink
-      key="pl"
+    <DropButton
       label="Platforms"
-      to="/platforms"
-      state={{ state: { isPlatformHeaderClicked: true } }}
+      dropAlign={{ top: 'bottom', left: 'left' }}
+      dropContent={
+        <>
+          <ButtonLink
+            key="pl"
+            label="All Platforms"
+            to="/platforms"
+            state={{ state: { isPlatformHeaderClicked: true } }}
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hecp"
+            label="HPE Ezmeral Container Platform"
+            to="/platform/hpe-ezmeral-container-platform/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hedf"
+            label="HPE Ezmeral Data Fabric"
+            to="/platform/hpe-ezmeral-data-fabric/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hgl"
+            label="HPE GreenLake"
+            to="/platform/hpe-greenlake/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-ssp"
+            label="SPIFFE and SPIRE Projects"
+            to="/platform/spiffe-and-spire-projects/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-c"
+            label="Chapel"
+            to="/platform/chapel/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-g"
+            label="Grommet"
+            to="/platform/grommet/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-ha"
+            label="HPE Alletra"
+            to="/platform/hpe-alletra/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hdlc"
+            label="HPE Deep Learning Cookbook"
+            to="/platform/hpe-deep-learning-cookbook/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-ss"
+            label="SmartSim"
+            to="/platform/smartsim/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-adh"
+            label="Aruba Developer Hub"
+            to="/platform/aruba/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-h3p"
+            label="HPE 3PAR and Primera"
+            to="/platform/hpe-3par-and-primera/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hns"
+            label="HPE Nimble Storage"
+            to="/platform/hpe-nimble-storage/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-ho"
+            label="HPE OneView"
+            to="/platform/hpe-oneview/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hogd"
+            label="HPE OneView Global Dashboard"
+            to="/platform/hpe-oneview-global-dashboard/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-hs"
+            label="HPE SimpliVity"
+            to="/platform/hpe-simplivity/home/"
+            alignSelf="start"
+          />
+          <ButtonLink
+            key="pl-ira"
+            label="iLO RESTful API"
+            to="/platform/ilo-restful-api/home/"
+            alignSelf="start"
+          />
+        </>
+      }
     />,
     <ButtonLink key="ev" label="Events" to="/events" />,
     <ButtonLink key="su" label="Skill Up" to="/skillup" />,
