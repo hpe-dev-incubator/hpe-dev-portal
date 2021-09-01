0d441b8f1d0f5a9b4d15ef7c8aa09ec537c36d87
Johnathan Giang <jayhgiang@gmail.com>
2021-09-01 10:46:19 -0700

    Update content in contribute page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -67,7 +67,7 @@ function Blog({ data, location }) {
         <Paragraph>
           Sharing expertise is a great way to move technology forward. Browse
           through our library of tutorials and articles to learn new ways to do
-          things. Or write your own!
+          things. Or, click on the Get Started button to write your own!
         </Paragraph>
         <Box wrap align="start">
           <ButtonLink primary label="Get Started" to="/contribute" />
