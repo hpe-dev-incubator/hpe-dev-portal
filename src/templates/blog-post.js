82ae563308630fab0efde9dcf6468fe82495759e
Chethan B <chethan-b.-@hpe.com>
2023-07-06 11:58:43 +0530

    external blog links fixed-2

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -185,11 +185,10 @@ function BlogPostTemplate({ data }) {
             )}
             <SectionHeader title="Related" color="border">
               <ResponsiveGrid gap="large" rows={rows} columns={columns}>
-                {/* {blogsByTags.map((blogPost) =>
-                  blogPost.url !== '/' &&
-                  (blogPost.node.frontmatter.authorimage ||
-                    blogPost.node.frontmatter.author) ? (
-
+                {blogsByTags.map((blogPost) =>
+                  // blogPost.url !== '/' &&
+                  blogPost.node.frontmatter.authorimage ||
+                  blogPost.node.frontmatter.author ? (
                     <BlogCard key={blogPost.node.id} node={blogPost.node} />
                   ) : undefined,
                 )} */}
