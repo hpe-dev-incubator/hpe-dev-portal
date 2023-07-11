570050f9d35119b0c766099ea5ba986b132a716b
Chethan B <chethan-b.-@hpe.com>
2023-07-11 15:30:49 +0530

    external-blog-link-issue-3

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -186,7 +186,7 @@ function BlogPostTemplate({ data }) {
             <SectionHeader title="Related" color="border">
               <ResponsiveGrid gap="large" rows={rows} columns={columns}>
                 {blogsByTags.map((blogPost) =>
-                  // blogPost.url !== '/' &&
+                  blogPost.url !== '/' &&
                   blogPost.node.frontmatter.authorimage ||
                   blogPost.node.frontmatter.author ? (
                     <BlogCard key={blogPost.node.id} node={blogPost.node} />
@@ -301,8 +301,6 @@ export const pageQuery = graphql`
             tags
             authorimage
             thumbnailimage
-            disable
-            externalLink
           }
           rawMarkdownBody
         }
